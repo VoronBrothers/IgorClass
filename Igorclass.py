54fd49816d54fedc81a210e2b3eb1cbee7664753
FirstPythonVB <Raven1217@mail.ru>
2023-01-12 22:47:07 +0300

    Fourth 12.01.2023

---
+++ Igorclass.py
@@ -0,0 +1,12 @@
+class Igor:
+
+
+
+    tall = 170
+
+    def voice(self):
+        print("Привет")
+
+    def age(self):
+        import datetime
+        return int(datetime.date.today().strftime("%Y")) - 1995
