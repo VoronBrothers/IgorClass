c432fedec2895affe3c52b339de898d814bdf925
FirstPythonVB <Raven1217@mail.ru>
2023-01-12 22:22:24 +0300

    Second 12.01.2023

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-print("hello python")
-print('')
-""
+a = None
+
+
+class Igor:
+    tall = 175
+
+    def voice(self):
+        print("Привет")
+
+
+while True:
+
+    a = input("Введите значение: ")
+
+    if a == "0":
+        print("Конец программы")
+        break
+    elif a == "1":
+        continue
+    else:
+        b = Igor()
+        print(b.tall)
+        b.voice()
+
+
